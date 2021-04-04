7f2c1fa369b097b876d4253b90598c65243418c0
yaya <papmalle@gmail.com>
2021-04-05 00:50:51 +0200

    add details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,8 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply test', 'https://cdn.radiofrance.fr/s3/cruiser-production/2019/05/29f8980b-e7e1-4f73-a14f-cc415d599134/870x489_maxwatier041179.webp'),
+    new Recipe('A Test Recipe', 'This is simply test', 'https://cdn.radiofrance.fr/s3/cruiser-production/2019/05/29f8980b-e7e1-4f73-a14f-cc415d599134/870x489_maxwatier041179.webp')
+  ];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,6 @@
+export class Ingredient {
+
+  // pas besoin de mettre les attributs il le fait automatique avec ce syntaxe
+  constructor(public name: string, public amount: number) {
+  }
+}
